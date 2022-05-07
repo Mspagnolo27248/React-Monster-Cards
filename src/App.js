f095bf137c7fb5f7aead50cff579c213af29fad8
Michael Spagnolo <mspagnolo27@gmail.com>
2022-05-06 22:01:14 -0400

    card list component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import {
+  Component
+} from 'react';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField:''
+
+    }
+    console.log('Constructor')
+  }
+
+  changeHandler() {
+    
+  }
+
+  componentDidMount() {
+    //console.log('ComponentDidMount')
+     fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((data) =>
+       this.setState(
+         () => {
+          return {monsters:data};
+      }, 
+      () => {
+        console.log(this.state)
+      }
+    ) 
   );
-}
+};
+
+  render() {
+    const filteredMonsters =  this.state.monsters.filter((item)=> 
+    (item.name.toLowerCase().includes(this.state.searchField)))
+
+    console.log('Render')
+      return ( 
+        <div className='App'>
+        <input
+        className='search-box'
+        type='search'
+        placeholder='search monsters'
+        onChange={(event)=>{
+          console.log(event.target.value)
+          const searchString = event.target.value.toLocaleLowerCase();
+          this.setState(()=>  ({searchField:searchString}))
+        }}
+        />
+
+       
+     <CardList monsters={filteredMonsters}/>
+        </div>
+      )
+    
+  }
+};
 
 export default App;
---
+++ src/searchBox.js
@@ -0,0 +1,14 @@
+import {useState} from "react"
+
+
+
+function searchBox(){
+
+
+
+    return ( <div>
+
+    </div>)
+}
+
+export default searchBox;
